45e52b3f345895af824848d08fad850ea54422c6
Marianne Linhares Monteiro <monteirom@monteirom.nyc.corp.google.com>
2017-06-16 14:04:31 -0400

    Updating distruted tensorflow folder

---
+++ code_samples/distributed_tensorflow/test_local/master/TF_CONFIG.py
@@ -0,0 +1,15 @@
+import json
+
+# set up de the environment
+cluster = {'master': ['localhost:2220'],
+           'ps': ['localhost:2222', 'localhost:2221'],
+           'worker': ['localhost:2223', 'localhost:2224']}
+
+TF_CONFIG = json.dumps(
+  {'cluster': cluster,
+   'task': {'type': 'master', 'index': 0},
+   'model_dir': '/tmp/output_test',
+   'environment': 'cloud'
+  })
+
+print(TF_CONFIG)
---
+++ code_samples/distributed_tensorflow/test_local/ps_1/TF_CONFIG.py
@@ -0,0 +1,15 @@
+import json
+
+# set up de the environment
+cluster = {'master': ['localhost:2220'],
+           'ps': ['localhost:2222', 'localhost:2221'],
+           'worker': ['localhost:2223', 'localhost:2224']}
+
+TF_CONFIG = json.dumps(
+  {'cluster': cluster,
+   'task': {'type': 'ps', 'index': 1},
+   'model_dir': '/tmp/output_test',
+   'environment': 'cloud'
+  })
+
+print(TF_CONFIG)
