957c2b54a8ff5e12195f96ed7c9849c90d7b971c
Marianne Linhares Monteiro <monteirom@monteirom.nyc.corp.google.com>
2017-07-05 14:23:38 -0400

    Adding bar plot to plot_gpu_use

--- code_samples/GPUs/plot_gpu_use.py
+++ code_samples/GPUs/plot_gpu_use.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import csv
 import argparse
+import numpy as np
 
 # parser
 parser = argparse.ArgumentParser('Plot GPU use')
@@ -10,7 +11,7 @@ args = parser.parse_args()
 with open(args.csv_path, 'r') as csvfile:
     plots = csv.reader(csvfile, delimiter=',')
     
-    y = [[r for r in row] for row in plots]
+    y = [[int(r) for r in row] for row in plots]
     x = range(len(y)) # number of iterations
     num_gpus = len(y[0]) # number of gpus
 
@@ -19,5 +20,17 @@ plt.legend(['gpu' + str(n) for n in range(num_gpus)])
 
 plt.xlabel('time')
 plt.ylabel('gpu use (%)')
+
+plt.show()
+
+def avg(x):
+	return sum(x)/(len(x) * 1.0)
+
+avg_y = np.mean(np.asarray(y), axis=0)
+y_pos = np.arange(num_gpus)
+plt.bar(y_pos, height=avg_y)
+plt.xticks(y_pos, ['gpu' + str(n) for n in range(num_gpus)])
+
+plt.ylabel('Average gpu use (%)')
 
 plt.show()
