14360eb081d5f17e2fe110b31c70be159ced4dc6
Marianne Linhares Monteiro <monteirom@google.com>
2017-07-13 13:14:38 -0400

    Updating tutorial

--- code_samples/RNN/sentiment_analysis/samples/tutorial.py
+++ code_samples/RNN/sentiment_analysis/samples/tutorial.py
@@ -8,6 +8,7 @@ from input_functions import get_input_fn
 from models import get_model_fn as CustomRNNEstimator
 import tensorflow as tf
 from tensorflow.contrib.keras.python.keras.datasets import imdb
+import random
 
 print('TensorFlow version', tf.__version__)
 
@@ -17,6 +18,11 @@ parser = argparse.ArgumentParser()
 parser.add_argument(
     '--model_dir', type=str, default='sentiment_analysis_output',
     help='The directory where the model outputs should be stored')
+
+parser.add_argument(
+    '--use_canned_estimator', type=bool, default=False,
+    help='If True the DynamicRNNEstimator will be loaded,'
+         ' otherwise the CustomRNNEstimator will be loaded')
 
 parser.add_argument(
     '--batch_by_seq_len', type=bool, default=False,
@@ -40,7 +46,7 @@ parser.add_argument(
     help='Batch size used for evaluation')
 
 parser.add_argument(
-    '--embed_dim', type=int, default=30,
+    '--embed_dim', type=int, default=128,
     help='Embedding dimension')
 
 parser.add_argument(
@@ -48,7 +54,7 @@ parser.add_argument(
     help='Learning rate')
 
 parser.add_argument(
-    '--num_epochs', type=int, default=10,
+    '--num_epochs', type=int, default=1,
     help='Num epochs used for training (for evaluation is always 1)')
 
 parser.add_argument(
@@ -60,7 +66,7 @@ parser.add_argument(
     help='Optimizer used for training')
 
 parser.add_argument(
-    '--num_rnn_units', nargs='+', type=int, default=[256, 128],
+    '--num_rnn_units', nargs='+', type=int, default=[128],
     help='Size of the hidden state for each RNN cell')
 
 parser.add_argument(
@@ -69,21 +75,44 @@ parser.add_argument(
 
 parser.add_argument(
     '--dropout_keep_probabilities', nargs='+', type=int,
-    default=[0.9, 0.9, 0.9],
+    default=[0.9],
     help='Dropout probabilities to keep the cell. '
-         'If provided the length should be num_rnn_units + 1')
+         'If provided the length should be num_rnn_units + 1'
+         'for the CannedEstimator and length = num_rnn_units'
+         'for the CustomEstimator')
 
 parser.add_argument(
     '--num_classes', type=int, default=2,
     help='Number of output classes. '
          'For sentiment analysis is 2 (positive and negative)')
 
+parser.add_argument(
+    '--index_from', type=int, default=3,
+    help='Index actual words with this index and higher')
+
+
 FLAGS = parser.parse_args()
 print(FLAGS)
 
 
-def main(unused_argv):
+def _load_map_dicts():
+  word_to_id = imdb.get_word_index()
+  word_to_id = {k: (v + FLAGS.index_from) for k, v in word_to_id.items()}
 
+  word_to_id["<PAD>"] = 0
+  word_to_id["<START>"] = 1
+  word_to_id["<UNK>"] = 2
+
+  id_to_word = {value: key for key, value in word_to_id.items()}
+
+  return word_to_id, id_to_word
+
+
+def _ids_to_sentence(sequence, id_to_word):
+  return ' '.join(id_to_word[id] for id in sequence)
+
+
+def main(unused_argv):
   # Loading the data
   # data from: https://keras.io/datasets/
   # Dataset of 25,000 movies reviews from IMDB, labeled by sentiment
@@ -99,22 +128,44 @@ def main(unused_argv):
   print('size of the train dataset:', x_train.shape[0])
   print('size of the test dataset:', x_test.shape[0])
 
-  # run experiment
+  # run config
   run_config = tf.estimator.RunConfig()
 
   # creates estimator
-  model_fn = CustomRNNEstimator(
-      rnn_cell_sizes=FLAGS.num_rnn_units,
-      label_dimension=FLAGS.num_classes,
-      num_words=FLAGS.num_words,
-      dnn_layer_sizes=FLAGS.num_dnn_units,
-      optimizer=FLAGS.optimizer,
-      learning_rate=FLAGS.learning_rate,
-      embed_dim=FLAGS.embed_dim)
+  if FLAGS.use_canned_estimator:
+    xc = tf.contrib.layers.sparse_column_with_integerized_feature(
+        'x',
+        FLAGS.num_words)
+    xc = tf.contrib.layers.embedding_column(xc, FLAGS.embed_dim)
 
-  estimator = tf.estimator.Estimator(model_fn=model_fn,
-                                     model_dir=FLAGS.model_dir,
-                                     config=run_config)
+    # creates estimator
+    estimator = tf.contrib.learn.DynamicRnnEstimator(
+        config=run_config,
+        model_dir=FLAGS.model_dir,
+        problem_type=constants.ProblemType.CLASSIFICATION,
+        prediction_type=PredictionType.SINGLE_VALUE,
+        sequence_feature_columns=[xc],
+        context_feature_columns=None,
+        num_units=FLAGS.num_rnn_units,
+        cell_type=FLAGS.cell_type,
+        optimizer=FLAGS.optimizer,
+        learning_rate=FLAGS.learning_rate,
+        num_classes=FLAGS.num_classes,
+        dropout_keep_probabilities=FLAGS.dropout_keep_probabilities)
+
+  else:
+    model_fn = CustomRNNEstimator(
+        rnn_cell_sizes=FLAGS.num_rnn_units,
+        label_dimension=FLAGS.num_classes,
+        num_words=FLAGS.num_words,
+        dnn_layer_sizes=FLAGS.num_dnn_units,
+        optimizer=FLAGS.optimizer,
+        learning_rate=FLAGS.learning_rate,
+        embed_dim=FLAGS.embed_dim)
+    estimator = tf.estimator.Estimator(model_fn=model_fn,
+                                       model_dir=FLAGS.model_dir,
+                                       config=run_config)
+
 
   # input functions
   train_input = get_input_fn(x_train, y_train, FLAGS.train_batch_size,
@@ -126,12 +177,24 @@ def main(unused_argv):
                             epochs=1,
                             max_length=FLAGS.max_len)
 
-
   # training
-  estimator.train(input_fn=train_input)
+  # estimator.train(input_fn=train_input)
 
   # evalutaion
-  estimator.evaluate(input_fn=test_input)
+  # estimator.evaluate(input_fn=test_input)
+
+  # predict
+  predictions = list(estimator.predict(input_fn=test_input))
+
+  # loading map from word to index and index to word
+  word_to_index, index_to_word = _load_map_dicts()
+
+  for i in range(5):
+    index = random.randint(0, x_test.shape[0]) # choose random index in the test dataset
+    print(_ids_to_sentence(x_test[index], index_to_word))
+    print('Prediction:', predictions[index])
+    print('Label:', y_test[index])
+
 
 if __name__ == '__main__':
   tf.logging.set_verbosity(tf.logging.INFO)  # enable TensorFlow logs
